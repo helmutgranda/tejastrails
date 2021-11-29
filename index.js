f6981a9d142a4e7227577808ed74ff06ed2b4d45
Helmut Granda <contact@helmutgranda.com>
2021-11-28 23:09:33 -0600

    Dynamic map with running logo.

--- index.js
+++ index.js
@@ -1,46 +1,84 @@
-// This example displays a marker at the center of Australia.
-// When the user clicks the marker, an info window opens.
-function initMap() {
-  const uluru = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  const contentString = `
+
+const locations = [
+  {
+  long:-99.2009815,
+  lat:29.6424935,
+  title: 'Bandera Endurance Trail Run',
+  races: `<i>100k</i>, 50k, 25k, Youth Trail Run`,
+  banner: `https://doc-08-bg-mymaps.googleusercontent.com/untrusted/hostedimage/q6hcfa588f6kpjujbjtckbce90/rjl68pu4318f141alt8guo2808/1638047879000/uHD6OSnEXJ-ke1CRL--DKLw47dcv6FCa/03400294955866176041/5AIsG_vbJkcyikHdPw5a9zc0gD9y8_cRqXx_bzqUo3sEJFdzTq_ZzavTX2FeMH9y4rUczpoKGJloFnDNyWabDkNuA5JPyUEutzGUIiCunwyE5smX55LDrUddM3E-IJ4nlbj80hec4Nm6gVY9qwRRpGm3C8l_E9zpHCk0g3tJLUOSS-A_jpRUrSx8hiRmZvktYwtQWwqo?session=0&fife`
+},
+{
+  long:-95.5223628,
+  lat:30.6209038,
+  title: 'Rocky 50',
+  month: 2,
+  races: `50m, 50k, 13.1m, Youth Trail`,
+  singup: `https://runsignup.com/Race/TX/Huntsville/Rocky50`,
+  banner: `https://doc-14-bg-mymaps.googleusercontent.com/untrusted/hostedimage/q6hcfa588f6kpjujbjtckbce90/av7ogpbraveaocpe5eodn3tpes/1638047879000/uHD6OSnEXJ-ke1CRL--DKLw47dcv6FCa/03400294955866176041/5AIsG_vabSqdVcShrjoBs18YidFFwaxRBTJDeitybm9yNSwf1qW3Jl7XxqxGIwrFLVMmhOlwb_OjZ-s5BzzK9TSz1WOPdr5GGh2RAqtO5MFZHJ_l1ApA59kIoOVVsjb_1o5c6OnlJqGm7GEDLVsrxXlXZMPiJ46OabSImjaPkt_Xr8AsNal5BtcfZFeS5fOfGaeST7N0?session=0&fife`
+},
+{
+  longt:-95.5223628,
+  lat:30.6209038,
+  title: 'Rocky Raccoon 100 Endurance Trail Run',
+  races: `100m, 100k, Youth Trail`,
+  singup: `https://runsignup.com/Race/Info/TX/Huntsville/RockyRaccoonEnduranceTrailRun#directions`,
+  banner: `https://doc-0k-bg-mymaps.googleusercontent.com/untrusted/hostedimage/q6hcfa588f6kpjujbjtckbce90/9vf4ikc5nucbngft35un47bfm4/1638047879000/uHD6OSnEXJ-ke1CRL--DKLw47dcv6FCa/03400294955866176041/5AIsG_vZIvwVfE8BbUgBVzHawLrEVhj0bUbN62nIckp07droQcEp9M9C5GPuai3jS6vefmxOBKVK9poAQcdptk1czch6ZvzPZS98qnQAI5xwpMX5XU5C-Jb0OEQmEargTULF63u-0Mdf9A1RcmnH0GhaHIPOTG1IN4Y8yJueleskFvpwRZebCDFCvcjFxr3DrlcTtVPQ?session=0&fife`
+},
+{
+  long:-99.9588639,
+  lat:29.9769323,
+  title: 'J&J Race and Trail Running Reunion',
+  races: `J&J101m, J&J100km, J&J42k, J&J21m, J&J 21m Ruck, J&J 10m, J&J 4m, J&J Youth Trail`,
+  month: 3,
+  singup: `https://runsignup.com/Race/TX/RockSprings/JJRaceandTrailRunningReunion`,
+  banner: `https://doc-10-bg-mymaps.googleusercontent.com/untrusted/hostedimage/q6hcfa588f6kpjujbjtckbce90/2b24gcr2b5gds33bj5l54b5qak/1638047879000/uHD6OSnEXJ-ke1CRL--DKLw47dcv6FCa/03400294955866176041/5AIsG_vaEHTQDT4WSinH8BuvS97M20q2GwLJ87_7U4kZzvjYGDnDl4YAUlo1ohIMynTzf7JDheaBeVE_dnhTsWuZL1AB6p2f0Dy-iYjf-3WuJGxoCB5j63PUGuuBJL1raTVxgHqAP2U0PF3c4-G0j9HD9Cpa1qmwxEk93rV54uo9S6u8BcAt11wZpcuPqYPif1ZQyDTg?session=0&fife`
+},
+]
+
+const icon = './running.png'
+
+function infoWindowContent (title, races, singup, banner ) {
+  return (
+    `
     <div id="content">
     <div id="siteNotice">
     </div>
-    <h1 id="firstHeading" class="firstHeading">Uluru</h1>
+    <h1 id="firstHeading" class="firstHeading">${title}</h1>
     <div id="bodyContent">
-    <p><b>Uluru2</b>, also referred to as <b>Ayers Rock</b>, is a large
-    sandstone rock formation in the southern part of the
-    Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)
-    south west of the nearest large town, Alice Springs; 450&#160;km
-    (280&#160;mi) by road. Kata Tjuta and Uluru are the two major
-    features of the Uluru - Kata Tjuta National Park. Uluru is
-    sacred to the Pitjantjatjara and Yankunytjatjara, the
-    Aboriginal people of the area. It has many springs, waterholes,
-    rock caves and ancient paintings. Uluru is listed as a World
-    Heritage Site.</p>
-    <p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
-    https://en.wikipedia.org/w/index.php?title=Uluru</a>
-    (last visited June 22, 2009).</p>
+    <p>${races}</p>
+    <p><a href="${singup}" target="_blank">
+    ${singup}</a></p>
+    <img src="${banner}" width="100px"/>
     </div>
-    </div>`;
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map,
-    title: "Uluru (Ayers Rock)",
+    </div>`
+  )
+}
+
+function initMap() {
+  
+  const austin = { lat: 30.270857128596095, lng: -97.75414001965449 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: austin,
   });
 
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
-      shouldFocus: false,
-    });
-  });
+  const infowindow = new google.maps.InfoWindow({});
+
+  locations.map((item) => {
+    const m = new google.maps.Marker({
+      position: {lat: item.lat, lng: item.long},
+      icon: icon,
+      map: map,
+      title: item.title
+    })
+    m.addListener("click", () => {
+      infowindow.setContent(infoWindowContent(item.title, item.races, item.singup, item.banner))
+      infowindow.open( {
+        anchor: m,
+        map: map,
+        shouldFocus: false,
+      })
+    })
+  })
+
 }
