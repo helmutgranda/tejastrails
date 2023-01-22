2dd7ff3d9cb07969a9d6099bc0a0395aeac7d313
Helmut Granda <contact@helmutgranda.com>
2023-01-21 20:30:25 -0600

    Added custom style

--- index.js
+++ index.js
@@ -277,6 +277,7 @@ function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: austin,
+    styles: [{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#C6E2FF"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#C5E3BF"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#D1D1B8"}]}],
   });
 
   const infowindow = new google.maps.InfoWindow({})
